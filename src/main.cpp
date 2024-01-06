877c7d2f1420efba29efc59b1e93910fcd01e7be
Владислав <nondefraudat@gmail.com>
2024-01-06 21:50:35 +0300

    OpenGL options

--- src/main.cpp
+++ src/main.cpp
@@ -144,11 +144,24 @@ int main() noexcept {
         return 0;
     }
 
+    // OpenGL options
+
+    glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
     // Init shaders
 
-    // Model
-
     const auto programId = loadShaders();
+
+    // Model
 
     // Main loop
 
