7d4ee26595ef7b04ad19b387a9370b2e6a14e19f
Владислав <nondefraudat@gmail.com>
2024-01-06 21:24:37 +0300

    Vertex and fragment shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+#include <type_traits>
 
 class GLFWRAII {
 public:
@@ -11,6 +17,98 @@ public:
         glfwTerminate();
     }
 };
+
+auto readAll(const char* const fileName) noexcept {
+    auto file = std::ifstream(fileName);
+    file.seekg(0, std::ios::end);
+    auto buffer = std::string(static_cast<size_t>(file.tellg()), '\0');
+    file.seekg(0, std::ios::beg);
+    buffer.assign((std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>());
+    return buffer;
+}
+
+auto compileShader(const int shaderFlag,
+        const char* const srcName) noexcept {
+    const auto shaderId = glCreateShader(shaderFlag);
+    auto fileContent = readAll(srcName);
+    const GLchar* shadersSrcs[] = { fileContent.data() };
+    glShaderSource(shaderId, 1, shadersSrcs, nullptr);
+    glCompileShader(shaderId);
+    return shaderId;
+}
+
+auto showShaderCompileError(const GLint shaderId) noexcept {
+    const auto bufferSize = 512;
+    char buffer[bufferSize];
+    glGetShaderInfoLog(shaderId,
+            bufferSize, nullptr, buffer);
+    std::cout << "Shader compiling error! Log:\n" <<
+            buffer << std::endl;
+}
+
+auto loadShader(const int shaderFlag,
+        const char* const srcName) noexcept {
+    const auto shaderId = compileShader(shaderFlag, srcName);
+    GLint success;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        showShaderCompileError(shaderId);
+        return static_cast<decltype(shaderId)>(-1);
+    }
+    return shaderId;
+}
+
+auto showProgramLinkError(const GLint programId) noexcept {
+    const auto bufferSize = 512;
+    char buffer[bufferSize];
+    glGetProgramInfoLog(programId,
+            bufferSize, nullptr, buffer);
+    std::cout << "Program linking error! Log:\n" <<
+            buffer << std::endl;
+}
+
+using RType = std::result_of_t<decltype(glCreateProgram)()>;
+
+auto loadShaders() noexcept {
+    // Load
+
+    const auto vertexShaderId = loadShader(
+            GL_VERTEX_SHADER, "shaders/vertexcore.glsl");
+    if (vertexShaderId == -1) {
+        glUseProgram(0u);
+        return static_cast<RType>(-1);
+    }
+    const auto fragmentShaderId = loadShader(
+            GL_FRAGMENT_SHADER, "shaders/fragmentcore.glsl");
+    if (fragmentShaderId == -1) {
+        glDeleteShader(vertexShaderId);
+        glUseProgram(0u);
+        return static_cast<RType>(-1);
+    }
+
+    // Program
+
+    const auto programId = glCreateProgram();
+    glAttachShader(programId, vertexShaderId);
+    glAttachShader(programId, fragmentShaderId);
+
+    glLinkProgram(programId);
+    
+    GLint success;
+    glGetProgramiv(programId, GL_LINK_STATUS, &success);
+    if (!success) {
+        showProgramLinkError(programId);
+    }
+
+
+    // Exit
+
+    glUseProgram(0u);
+    glDeleteShader(vertexShaderId);
+    glDeleteShader(fragmentShaderId);
+    return programId;
+}
 
 int main() noexcept {
     // Init GLFW
@@ -46,6 +144,12 @@ int main() noexcept {
         return 0;
     }
 
+    // Init shaders
+
+    // Model
+
+    const auto programId = loadShaders();
+
     // Main loop
 
     while (!glfwWindowShouldClose(window)) {
@@ -66,5 +170,6 @@ int main() noexcept {
 
     // End of program
 
+    glfwDestroyWindow(window);
     return 0;
 }
